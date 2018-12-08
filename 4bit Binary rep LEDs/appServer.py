664fdcc916345ed5effad9bc40b6ef11b2f95aad
kshtjtcs <kshtjtcs@outlook.com>
2018-12-08 20:57:43 +0000

    Working code added

--- 4bit Binary rep LEDs/appServer.py
+++ 4bit Binary rep LEDs/appServer.py
@@ -1,6 +1,45 @@
-from flask import Flask, render_template, redirect, request, url_for
 import RPi.GPIO as gpio
 
+#Code to deal with LEDs representation
+def convertToBinary(number):
+
+	returnable = ''
+
+	while(number != 0):
+
+		returnable += str(number % 2)
+
+		number = number // 2
+
+	return returnable[::-1]
+
+#Using the GPIO pins 3, 5, 7, 11 for LEDs
+def displayInLEDs(binary):
+
+	pinList = [3, 5, 7, 11]
+
+	for i in range(0, len(binary) - 1):
+
+		gpio.output(pinList[i], int(binary[i]))
+
+#Calling code
+def callingCode(operableValue):
+	gpio.cleanup()
+	gpio.setmode(gpio.BOARD)
+	gpio.setup(3, gpio.OUT)
+	gpio.setup(5, gpio.OUT)
+	gpio.setup(7, gpio.OUT)
+	gpio.setup(11, gpio.OUT)
+	# print("Enter a 4 bit number to convert to binary")
+	operableValue = convertToBinary(int(operableValue))
+	print(type(operableValue))
+	displayInLEDs(operableValue)
+
+
+
+
+#Web app code
+from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
 
 
@@ -27,41 +66,3 @@ def formPost():
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=80)
-
-
-#Code to deal with LEDs representation
-def convertToBinary(number):
-
-		returnable = ''
-
-		while(number != 0):
-
-			returnable += str(number % 2)
-
-			number = number // 2
-
-		return returnable[::-1]
-
-	#Using the GPIO pins 3, 5, 7, 11 for LEDs
-
-def displayInLEDs(binary):
-
-	pinList = [3, 5, 7, 11]
-
-	for i in range(0, len(binary) - 1):
-
-		gpio.output(pinList[i], int(binary[i]))
-
-
-	#Calling code
-def callingCode(operableValue):
-	gpio.cleanup()
-	gpio.setmode(gpio.BOARD)
-	gpio.setup(3, gpio.OUT)
-	gpio.setup(5, gpio.OUT)
-	gpio.setup(7, gpio.OUT)
-	gpio.setup(11, gpio.OUT)
-	# print("Enter a 4 bit number to convert to binary")
-	operableValue = convertToBinary(int(operableValue))
-	print(type(operableValue))
-	displayInLEDs(, operableValue)
