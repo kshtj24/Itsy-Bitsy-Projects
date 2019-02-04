0b139e98453a8eb8e1a5ab60ddef6b1277c73df0
kshtj24 <kshitij.a.saxena@accenture.com>
2019-02-04 15:06:15 +0530

    test commit

---
+++ Algorithms study material/Test folder/PartyInvites/Controllers/InvitationController.cs
@@ -0,0 +1,32 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class InvitationController : Controller
+    {
+        // GET: Invitation
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Evening";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RSVPForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("Thanks",guestResponse);
+        }
+    }
+}
