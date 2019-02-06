e8228797e8c17ddf198b0076afe5ff44939c07f5
kshtj24 <kshtjtcs@outlook.com>
2019-02-06 15:06:36 +0530

    jump search in python added

--- Algorithms study material/Jump Search.py
+++ Algorithms study material/Jump Search.py
@@ -1,2 +1,19 @@
-def jumpSearch(arr, m, key):
-    
+from math import sqrt
+
+def jumpSearch(arr, m, key, index):
+    if(arr[index] == key):
+        return index
+    elif(arr[index] > key):
+        for i in range(index - m, index):
+            if(arr[i] == key):
+                return i
+    elif(arr[index] < key):
+        return jumpSearch(arr,m,key,index+m)
+    else:
+        return -1
+
+
+arr = list(map(int,input("Enter sorted array:").split(' ')))
+key = int(input('Enter search key'))
+
+print(jumpSearch(arr,int(sqrt(len(arr))),key,0))
