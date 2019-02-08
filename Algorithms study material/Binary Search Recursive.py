7c057fb6169d6d9a3ceb0d3cd5d7eea115a35d17
Acc Team <kshtjtcs@outlook.com>
2019-02-08 16:25:12 +0530

    changes in binary search algo

--- Algorithms study material/Binary Search Recursive.py
+++ Algorithms study material/Binary Search Recursive.py
@@ -7,6 +7,8 @@ def binary_search(arr,key,L,R):
             return binary_search(arr,key,L,mid-1)
         elif key > arr[mid]:
             return binary_search(arr,key,mid+1,R)
+		else:
+			return -1
 
 print("enter sorted array elements")
 arr = list(map(int,input().split(' ')))
