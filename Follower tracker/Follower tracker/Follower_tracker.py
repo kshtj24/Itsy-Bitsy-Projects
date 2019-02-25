55723d8f9b21cdab0f9d57d1ab614db7173ea554
kshtj24 <kshtjtcs@outlook.com>
2019-02-25 11:55:07 +0530

    updated

--- Follower tracker/Follower tracker/Follower_tracker.py
+++ Follower tracker/Follower tracker/Follower_tracker.py
@@ -1,7 +1,24 @@
 import tkinter as tk
 
+
+def getFollowersFromGit():
+
+
+
+
 mainWindow = tk.Tk()
 
+pane1 = tk.PanedWindow(mainWindow,orient='horizontal')#,height=100,width=100,bg='black')
+pane2 = tk.PanedWindow(mainWindow, orient = 'horizontal')#,height=100,width=100,bg='blue')
 
-listView = tk.Listbox(canvas,)
+oldList = tk.Listbox(pane1)
+newList = tk.Listbox(pane1)
+updateFollowers = tk.Button(pane2,text='Get followers')
+
+pane1.add(oldList)
+pane1.add(newList)
+pane2.add(updateFollowers)
+
+pane1.pack()
+pane2.pack()
 mainWindow.mainloop()
