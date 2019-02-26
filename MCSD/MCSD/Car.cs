6ab65fa5a74a4a192b01c1cb182921b7a930508b
Acc Team <kshtjtcs@outlook.com>
2019-02-26 18:20:26 +0530

    new folder added

---
+++ MCSD/MCSD/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCSD
+{
+    class Car : IComparable<Car>
+    {
+        public string Name { get; set; }
+        public int MaxMph { get; set; }
+        public int HorsePower { get; set; }
+        public decimal Price { get; set; }
+
+        public int CompareTo(Car obj)
+        {
+            Car other = obj as Car;
+            return Name.CompareTo(other.Name);         
+        }
+    }
+
+    class CarComparer : IComparer<Car>
+    {
+        public enum CompareField { Name, MaxMph, HorsePower, Price }
+
+        public CompareField SortBy = CompareField.Name;
+
+        public int Compare(Car x, Car y) 
+        {
+            switch (SortBy) 
+            {
+                case CompareField.Name:
+                    return x.Name.CompareTo(y.Name);
+                    break;
+                case CompareField.MaxMph:
+                    return x.MaxMph.CompareTo(y.MaxMph);
+                    break;
+                case CompareField.HorsePower:
+                    return x.HorsePower.CompareTo(y.HorsePower);
+                    break;
+                case CompareField.Price:
+                    return x.Price.CompareTo(y.Price);
+                    break;
+            }
+        }
+    }
+
+}
---
+++ MCSD/MCSD/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = 
+            {
+                new Car { Name = "SSC ultimate aero", MaxMph = 257, HorsePower = 1183, Price = 654400m },
+                new Car{ Name = "Bugati Veyron", MaxMph = 253, HorsePower = 1001, Price = 1700000m },
+                new Car{ Name = "Maruti 800", MaxMph = 180, HorsePower = 300, Price =1000m },
+                new Car{ Name = "Porshe Spyder", MaxMph =200 , HorsePower = 788, Price = 20809m},
+                new Car{ Name = "Wagon", MaxMph = 50, HorsePower =2 , Price =100m }                
+            };
+
+            displayCars(cars);
+            Console.ReadKey();
+        }
+
+        static void displayCars(Car[] cars)
+        {
+            Array.Sort(cars);
+
+            foreach (Car car in cars)
+                Console.WriteLine(string.Join(",", new string[] { car.Name, car.MaxMph.ToString(), car.HorsePower.ToString(), car.Price.ToString("c") }));
+        }
+    }
+}
