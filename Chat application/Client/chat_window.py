58cfce58a588cec837c9a3769a64e72f146eb617
Acc Team <kshtjtcs@outlook.com>
2019-04-01 16:21:50 +0530

    chat app updated

--- Chat application/Client/chat_window.py
+++ Chat application/Client/chat_window.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 
 def create_chat_window():
-	main_window = tk.Tk()
-	main_window.mainloop()
-	
+    main_window = tk.Tk()
+    main_window.geometry('420x640')
+    main_window.title('Chat window')
+    create_chat_controls(main_window)
+    main_window.mainloop()
 
-def create_chat_controls():
-	
+def create_chat_controls(window):
+    top_frame = tk.Frame(window,relief=tk.GROOVE, bg = 'green', height=500, width=400)
+    bottom_frame = tk.Frame(window,bg='white', height=100, width=400)
+    conversation_view = tk.Listbox(top_frame)
+    top_frame.place(x=10, y=10)
+    bottom_frame.place(x=10, y=510)
 
 if __name__ == '__main__':
-	
+	create_chat_window()
+    
