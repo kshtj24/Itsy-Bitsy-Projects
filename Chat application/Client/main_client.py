0b9848d59e77de8c458c7664d6f60c2711fea69a
Acc Team <kshtjtcs@outlook.com>
2019-04-01 16:22:16 +0530

    chat app updated

--- Chat application/Client/main_client.py
+++ Chat application/Client/main_client.py
@@ -8,15 +8,3 @@ HOST, PORT = 'localhost', 9999
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mSocket:
     mSocket.connect((HOST, PORT))
     mSocket.sendall(input().encode())
-
-
-
-canvas = Canvas(top_frame)
-    chat_frame = Frame(canvas)
-    
-    v_scroller = Scrollbar(top_frame,orient='vertical',command=canvas.yview)
-    canvas.configure(yscrollcommand=v_scroller.set)
-    
-    
-    v_scroller.pack(side='right',fill='y')
-    canvas.pack(side='left')
