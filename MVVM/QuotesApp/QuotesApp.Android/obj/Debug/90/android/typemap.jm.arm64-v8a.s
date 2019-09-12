01aee5b27bee9960b4f64d0fcc14018753d74e35
Kshitij Saxena <kshitij.a.saxena@accenture.com>
2019-09-12 12:38:30 +0530

    changes made

---
+++ MVVM/QuotesApp/QuotesApp.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: b4346178516acf814950a0da08e0d0a8f5e7a2e4 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1098
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 287677
+	.include	"typemap.jm.inc"
---
+++ MVVM/QuotesApp/QuotesApp/ViewModels/WelcomePageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuotesApp.ViewModels
+{
+    public class WelcomePageViewModel : BindableBase
+    {
+        public DelegateCommand LoginCommand { get; set; }
+        public INavigationService NavigationService { get; set; }
+
+        public WelcomePageViewModel(INavigationService navigationService)
+        {
+            LoginCommand = new DelegateCommand(LoginMethod);
+            NavigationService = navigationService;
+        }
+
+        private void LoginMethod()
+        {
+            NavigationService.NavigateAsync("HomePage");
+        }
+    }
+}
